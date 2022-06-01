571f503074068c8a7bec6ff247309f7b001b97b3
willyoliv <willyoliversilva@gmail.com>
2022-06-01 14:45:20 -0300

    adicionando annotation entity na class product

--- src/main/java/com/api/products/entity/Product.java
+++ src/main/java/com/api/products/entity/Product.java
@@ -5,14 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private UUID productCode;
     private String name;
