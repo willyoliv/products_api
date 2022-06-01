d78d1db1915026bf343a418b1525243cb5673619
willyoliv <willyoliversilva@gmail.com>
2022-06-01 09:15:17 -0300

    adicionando configurações do eureka client

--- src/main/java/com/api/products/entity/Product.java
+++ src/main/java/com/api/products/entity/Product.java
@@ -1,22 +1,18 @@
 package com.api.products.entity;
 
-import lombok.*;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private UUID productCode;
     private String name;
